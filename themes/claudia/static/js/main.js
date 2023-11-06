601e6fbb40a88c705230e0b263c781be5a66a666
birdflytomoon <yejia.yang@mycloudpa.com>
2023-11-06 16:07:08 +0000

    anchor nav

--- themes/claudia/static/js/main.js
+++ themes/claudia/static/js/main.js
@@ -59,7 +59,6 @@ if(copyBTN){
   containerEl.appendChild(copyBtn);
 }
 
-// Add copy button to code blocks
 let highlightBlocks = document.getElementsByClassName('highlight');
 Array.prototype.forEach.call(highlightBlocks, addCopyButton);
 
@@ -67,170 +66,6 @@ Array.prototype.forEach.call(highlightBlocks, addCopyButton);
 }
 
 
-
-
-
-  let particles = select('#particles-js')
-  if(particles){
-    particlesJS("", {
-      "particles": {
-        "number": {
-          "value": 90,
-          "density": {
-            "enable": true,
-            "value_area": 800
-          }
-        },
-        "color": {
-          "value": "#ffffff"
-        },
-        "shape": {
-          "type": "circle",
-          "stroke": {
-            "width": 0,
-            "color": "#000000"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-          "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
-          }
-        },
-        "opacity": {
-          "value": 0.1,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 3,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
-          "enable": true,
-          "distance": 150,
-          "color": "#ffffff",
-          "opacity": 0.2,
-          "width": 1
-        },
-        "move": {
-          "enable": true,
-          "speed": 6,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
-        }
-      },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": true,
-            "mode": "grab"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 140,
-            "line_linked": {
-              "opacity": 0.2
-            }
-          },
-          "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 0.2,
-            "speed": 3
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 4
-          },
-          "remove": {
-            "particles_nb": 2
-          }
-        }
-      },
-      "retina_detect": true
-    });
-  }
-
-
-
-        // Define selector for selecting
-      // anchor links with the hash
-      let anchorSelector = 'a[href^="#"]';
-      $(anchorSelector).on("click", function (e) {
-        // Prevent scrolling if the
-        // hash value is blank
-        e.preventDefault();
-
-        // Get the destination to scroll to
-        // using the hash property
-        let destination = $(this.hash);
-
-        // Get the position of the destination
-        // using the coordinates returned by
-        // offset() method and subtracting 50px
-        // from it.
-        let scrollPosition = destination.offset().top - 88;
-
-        // Specify animation duration
-        let animationDuration = 500;
-
-        // Animate the html/body with
-        // the scrollTop() method
-        $("html, body").animate(
-          {
-            scrollTop: scrollPosition,
-          },
-          animationDuration
-        );
-      });
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
- 
-  const jsyear = select('#js-year')
-  if(jsyear){
-    const date = new Date()
-    $("#js-year").html(date.getFullYear())
-  }
- 
- 
- 
 
   Fancybox.bind('[data-fancybox="gallery"]', {
     //
@@ -285,6 +120,47 @@ Array.prototype.forEach.call(highlightBlocks, addCopyButton);
       backDelay: 2000
     });
   }
+
+
+  console.info("start")
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                $(".nav > li > a").removeClass("newClass");
+                $(this).addClass("newClass");
+                return false;
+            }
+        }
+    });
+    $(window).scroll(function(){
+        $("section").each(function(){
+            if(ScrollView($(this))){
+                var id = $(this).attr("id");
+                $("#menu li").removeClass("newClass");
+                $("#menu     a[href='#" + id + "']  li ").addClass("newClass");
+            }
+        });
+    });
+    function ScrollView(element){
+        var win = $(window);
+        var winTop = win.scrollTop();
+        var winBottom = winTop + win.height();
+        var elementTop = element.offset().top;
+        var elementBottom = elementTop + element.height();
+
+        if((elementBottom <= winBottom) && (elementTop >= winTop)){
+            return true;
+        }
+        return false;
+    }
+
+
+
 })()
 listenerUrl()
 const pass = "123"
@@ -357,3 +233,4 @@ function closePassBox(type){
   }
  
 }
+
